e667c90c83abdf6c703bd0f1bf512f5da39e0bf8
robmccormack89 <robertmccormack147@gmail.com>
2021-06-25 14:01:10 +0100

    fix - synced to demo.robertmcc... site

--- inc/functions.php
+++ inc/functions.php
@@ -2,15 +2,21 @@
 
 function get_relateds_upsells() {
   $context = Timber::context();
+  $timber_post = Timber::get_post();
+  $context['post'] = $timber_post;
 
   // get relateds
   $related_limit = 12;
-  $related_ids = wc_get_related_products($context['post']->id, $related_limit);
-  $context['related_products'] = null;
-  if ($related_ids) $context['related_products'] = Timber::get_posts($related_ids);
+  $related_ids = wc_get_related_products($timber_post->id, $related_limit);
+  
+  if ($related_ids) {
+	$context['related_products'] = Timber::get_posts($related_ids);
+  } else {
+	$context['related_products'] = null;
+  }
   
   // get upsells
-  $upsell_ids = $context['post']->_upsell_ids;
+  $upsell_ids = $timber_post->_upsell_ids;
   $context['up_sells'] = null;
   if ($upsell_ids) $context['up_sells'] = Timber::get_posts($upsell_ids);
   
